cbfcaa086c5664a49ccb404ee2b9f56b866e6372
Matthew Philps <philps.matthew@gmail.com>
2019-03-13 07:35:51 +0900

    posts rendering

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,8 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder'
 
-export const fetchPosts = () => {
-    return async function(dispatch, getState){
+export const fetchPosts = () => async dispatch => {
         const response = await jsonPlaceholder.get('/posts')
 
-        dispatch({type:'FETCH_POSTS', payload:response})
+        dispatch({type:'FETCH_POSTS', payload:response.data})
     }
     
-}
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,6 @@
 import { combineReducers } from 'redux'
+import postsReducer from './postsReducer'
 
 export default combineReducers({
-    replaceMe: ()=> 'place holder until write first reducer'
+    posts: postsReducer 
 })
