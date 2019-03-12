76fb7b53cbd8320aae56e8522a3c5963a558297a
Matthew Philps <philps.matthew@gmail.com>
2019-03-12 10:08:17 +0900

    fetchPosts wired up

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,10 @@
+import jsonPlaceholder from '../apis/jsonPlaceholder'
+
 export const fetchPosts = () => {
-    return {
-        type: 'FETCH_POSTS'
+    return async function(dispatch, getState){
+        const response = await jsonPlaceholder.get('/posts')
+
+        dispatch({type:'FETCH_POSTS', payload:response})
     }
+    
 }
